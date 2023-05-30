30c7e8bbd0808d312a66d14c08bf5bd4340343f7
Rafał Mielniczuk <rmielniczuk001@gmail.com>
2023-05-30 19:33:47 +0200

    final v2

--- mini_projekt/build/CMakeFiles/main.dir/CMakeFiles/3.25.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/main.dir/CMakeFiles/3.25.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o: \
-  /Users/frafau/Documents/C/Techniki/mini_projekt/build/CMakeFiles/3.25.1/CompilerIdCXX/CMakeCXXCompilerId.cpp
